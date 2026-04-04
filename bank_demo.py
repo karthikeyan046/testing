035ff1aad11d463d3dcb4cbfa38d7d132cce668d
karthikeyan046 <karthikeyan046@yahoo.co.in>
2026-04-04 15:50:59 +0530

    Update bank_demo.py

--- bank_demo.py
+++ bank_demo.py
@@ -1,7 +1,7 @@
 # Simulate account operations
 
 def view_balance():
-    print("Your balance for the acoount is")
+    print("Your balance for the acoount is88")
 
 def transfer_funds(amount):
     print(f"Transferred ${amount} successfully!")
